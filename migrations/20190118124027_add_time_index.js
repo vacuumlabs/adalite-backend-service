f8385d7559a34838d93dc23f63b5033196e459a2
refi93 <rafael.korbas@gmail.com>
2019-08-23 02:36:08 +0200

    remove db migrations

--- migrations/20190118124027_add_time_index.js
+++
@@ -1,8 +0,0 @@
-exports.up = async (knex) => knex.schema.table('txs', (table) => {
-  table.index('time')
-})
-
-exports.down = async (knex) => knex.schema.table('txs', (table) => {
-  table.dropIndex('time')
-})
-
