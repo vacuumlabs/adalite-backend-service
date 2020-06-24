f88ac4c9ec54d1dd467edc8e1e74e64a5161c7ee
xdzurman <kamil.dzurman@gmail.com>
2020-06-24 11:51:32 +0200

    Hardcode submit api url suffix to code

--- src/importer-api.js
+++ src/importer-api.js
@@ -4,5 +4,13 @@ import { post } from 'axios'
 import type { ImporterApi } from 'icarus-backend'; // eslint-disable-line
 
 export default (importerSendTxEndpoint: string): ImporterApi => ({
-  sendTx: tx => post(importerSendTxEndpoint, tx),
+  sendTx: tx => post(
+    `${importerSendTxEndpoint}/api/submit/tx`,
+    tx,
+    {
+      headers: {
+        'Content-Type': 'application/cbor',
+      },
+    },
+  ),
 })
